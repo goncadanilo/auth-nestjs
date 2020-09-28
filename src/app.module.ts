afa2fc1517f22a41bde2559af01eb1fde266fc5e
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-09-28 09:52:03 -0300

    chore: set config module in the app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
-@Module({})
+@Module({
+  imports: [ConfigModule.forRoot()],
+})
 export class AppModule {}
