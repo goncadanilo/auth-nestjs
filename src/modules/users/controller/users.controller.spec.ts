e7b67545953fcd8bb5e81a7cb040f4f645ede460
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-09-28 13:32:05 -0300

    test: should be defined users controller

---
+++ src/modules/users/controller/users.controller.spec.ts
@@ -0,0 +1,22 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from '../service/users.service';
+import { UsersController } from './users.controller';
+
+describe('UsersController', () => {
+  let controller: UsersController;
+
+  const mockUsersService = {};
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [{ provide: UsersService, useValue: mockUsersService }],
+    }).compile();
+
+    controller = module.get<UsersController>(UsersController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
