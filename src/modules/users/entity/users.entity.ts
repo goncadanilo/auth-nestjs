7e0f16b3660d72ad005074cfdbe0c24d8ef6763b
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-09-28 11:14:13 -0300

    feat: create users entity

---
+++ src/modules/users/entity/users.entity.ts
@@ -0,0 +1,24 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Users {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+}
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from './entity/users.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Users])],
+})
 export class UsersModule {}
