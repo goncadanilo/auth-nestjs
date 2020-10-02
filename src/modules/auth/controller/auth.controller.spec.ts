e315656970762aef1e0b912700891b5c9d83f13c
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-10-02 11:15:03 -0300

    test: should authentication user and return an authentication token

--- src/modules/auth/controller/auth.controller.spec.ts
+++ src/modules/auth/controller/auth.controller.spec.ts
@@ -1,11 +1,16 @@
 import { Test } from '@nestjs/testing';
+import { TestUtil } from '../../../../test/utils/test.uttil';
+import { Users } from '../../users/entity/users.entity';
 import { AuthService } from '../service/auth.service';
 import { AuthController } from './auth.controller';
 
 describe('AuthController', () => {
   let authController: AuthController;
+  let mockUser: Users;
 
-  const mockAuthService = {};
+  const mockAuthService = {
+    login: jest.fn(),
+  };
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -14,9 +19,30 @@ describe('AuthController', () => {
     }).compile();
 
     authController = moduleRef.get<AuthController>(AuthController);
+    mockUser = TestUtil.getMockUser();
+  });
+
+  beforeEach(() => {
+    mockAuthService.login.mockReset();
   });
 
   it('should be defined', () => {
     expect(authController).toBeDefined();
+  });
+
+  describe('when login', () => {
+    it('should authentication user and return an authentication token', async () => {
+      mockAuthService.login.mockReturnValue({ token: 'valid_token' });
+
+      const user = {
+        id: mockUser.id,
+        email: mockUser.email,
+      };
+
+      const result = await authController.login(user);
+
+      expect(result).toHaveProperty('token', 'valid_token');
+      expect(mockAuthService.login).toBeCalledTimes(1);
+    });
   });
 });
