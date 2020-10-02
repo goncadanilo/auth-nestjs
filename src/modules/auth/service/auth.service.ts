aa71d2e87ea0fb11293c165e280bb6dfd8c54607
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-10-02 09:54:58 -0300

    feat: validate user

--- src/modules/auth/service/auth.service.ts
+++ src/modules/auth/service/auth.service.ts
@@ -1,7 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { CryptoService } from '../../../shared/services/crypto.service';
 import { UsersService } from '../../users/service/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private cryptoService: CryptoService,
+  ) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.usersService.findUserByEmail(email);
+    const correctPassword = await this.cryptoService.compareHash(
+      password,
+      user?.password,
+    );
+
+    if (correctPassword) {
+      return { id: user.id, email: user.email };
+    }
+
+    return null;
+  }
 }
