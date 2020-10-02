30184fb80844987e9c3ac55c6181f7e611be72b0
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-10-02 10:40:10 -0300

    feat: create jwt strategy

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UsersService } from '../users/service/users.service';
 import { AuthController } from './controller/auth.controller';
 import { AuthService } from './service/auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-  imports: [UsersService, PassportModule],
+  imports: [
+    UsersService,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET_KEY,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
