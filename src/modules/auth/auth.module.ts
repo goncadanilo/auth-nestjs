ce769859616655e814c3c9ca9a870482ab53dd88
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-10-02 11:50:57 -0300

    refactor: set crypto service in the users module providers and auth module providers

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { UsersService } from '../users/service/users.service';
+import { CryptoService } from '../../shared/services/crypto.service';
+import { UsersModule } from '../users/users.module';
 import { AuthController } from './controller/auth.controller';
 import { AuthService } from './service/auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
@@ -9,14 +11,15 @@ import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   imports: [
-    UsersService,
+    UsersModule,
     PassportModule,
+    ConfigModule.forRoot(),
     JwtModule.register({
       secret: process.env.JWT_SECRET_KEY,
       signOptions: { expiresIn: '1d' },
     }),
   ],
+  providers: [AuthService, LocalStrategy, JwtStrategy, CryptoService],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CryptoService } from '../../shared/services/crypto.service';
 import { UsersController } from './controller/users.controller';
 import { Users } from './entity/users.entity';
 import { UsersService } from './service/users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Users])],
-  providers: [UsersService],
+  providers: [UsersService, CryptoService],
   controllers: [UsersController],
   exports: [UsersService],
 })
